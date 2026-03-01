67ae5bd10a910cb62f70595fb8294bee970c92db
sevvalbakirhan <sevvalbakirhan@gmail.com>
2026-03-02 00:00:16 +0300

    First upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
+});
+
+const modal = document.getElementById("cert-modal");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+const span = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll('.certificate-box .btn').forEach(button => {
+    button.onclick = function (e) {
+        e.preventDefault();
+        modal.style.display = "block";
+        const imgSrc = this.parentElement.parentElement.querySelector('img').src;
+        const title = this.parentElement.querySelector('h3').innerText;
+
+        modalImg.src = imgSrc;
+        captionText.innerHTML = title;
+    }
+});
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
